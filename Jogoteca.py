22b4e0cf5fd165b0e1a400b670c6e40f05e4bde3
Lucas Barros <lucas.mr.barros@live.com>
2023-06-21 16:48:16 -0300

    Integração com banco de dados mysql

--- Jogoteca.py
+++ Jogoteca.py
@@ -1,46 +1,68 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
-
-
-class Jogo:
-    def __init__(self, nome, categoria, console):
-        self.nome = nome
-        self.categoria=categoria
-        self.console = console
-class Usuario:
-    def __init__(self, nome, nickname, senha):
-        self.nome = nome
-        self.nickname = nickname
-        self.senha = senha
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'ferrari'
 
-jogo1 = Jogo('F1 2013', 'Corrida', 'xbox 360')
-jogo2 = Jogo('Halo4', 'Tiro', 'Xbox one')
-jogo3 = Jogo('Flight Simulator 2020', 'Simulação', 'Xbox Series s/x')
-lista = [jogo1, jogo2, jogo3]
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+        SGBD='mysql+mysqlconnector',
+        usuario='root',
+        senha='admin',
+        servidor='127.0.0.1',
+        database='jogoteca'
+    )
 
-usuario1 = Usuario("Lucas", "LB", "scuderia")
-usuario2 = Usuario("Andressa", "andy", "louis")
+db = SQLAlchemy(app)
 
-usuarios= {usuario1.nickname : usuario1, usuario2.nickname : usuario2}
+class Jogos(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(50), nullable=False)
+    categoria = db.Column(db.String(40), nullable=False)
+    console = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+class Usuarios(db.Model):
+    nome = db.Column(db.String(20), nullable=False)
+    nickname = db.Column(db.String(8), primary_key=True, nullable=False)
+    senha = db.Column(db.String(100), nullable=True)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
 @app.route('/')
 def index():
+    lista = Jogos.query.order_by(Jogos.id)
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 @app.route('/novo')
 def novo():
-    if 'usuario_logado' not  in session or session['usuario_logado'] == None:
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('novo')))
-    return render_template('novo.html', titulo = 'Novo Jogo')
+
+    return render_template('novo.html', titulo='Novo Jogo')
 
 @app.route('/adicionar', methods=['POST', ])
 def adicionar():
     nome = request.form['nome']
     categoria = request.form['categoria']
     console = request.form['console']
-    jogo = Jogo(nome, categoria, console)
-    lista.append(jogo)
+
+    jogo = Jogos.query.filter_by(nome=nome).first()
+
+    if jogo:
+        flash('o jogo já existente!')
+
+    else:
+        novo_jogo = Jogos(nome=nome, categoria=categoria, console=console)
+
+        db.session.add(novo_jogo)
+        db.session.commit()
+
+        flash('Jogo ' + nome + ' adicionado com sucesso!')
+
     return redirect(url_for('index'))
 
 @app.route('/login')
@@ -50,11 +72,12 @@ def login():
 
 @app.route('/autenticar', methods=['POST', ])
 def autenticar():
-    if request.form['usuario'] in usuarios:
-        usuario = usuarios[request.form['usuario']]
+    usuario = Usuarios.query.filter_by(nickname=request.form['usuario']).first()
+
+    if usuario:
         if request.form['senha'] == usuario.senha:
             session['usuario_logado'] = usuario.nickname
-            flash('Usuário ' + session['usuario_logado'] + ' com sucesso')
+            flash('Usuário ' + session['usuario_logado'] + ' logado com sucesso')
             proxima_pagina = request.form['proxima']
             return redirect(proxima_pagina)
     else:
@@ -66,5 +89,4 @@ def logout():
     session['usuario_logado'] = None
     flash('usuario deslogado com sucesso')
     return redirect(url_for('index'))
-
 app.run(debug=True)
